d3a069b8cc159f99143ce46629e801c085374f97
Glitch (handsfree-robots) <none>
2019-01-28 23:51:59 +0000

    🐾💧 Checkpoint ./handsfree.js:1601221/6

--- handsfree.js
+++ handsfree.js
@@ -11,7 +11,7 @@ handsfree.use({
     poses.forEach(pose => {
       robot.position.x.setValue(pose.face.translationX / handsfree.debug.$canvas.width * 30 - 15)
       robot.position.y.setValue(20)
-      robot.position.z.setValue(-pose.face.translationY / handsfree.debug.$canvas.height * 40 + 30)
+      robot.position.z.setValue(-pose.face.translationY / handsfree.debug.$canvas.height * 35 + 20)
       // robot.position.y.setValue(pose.face.translationY / handsfree.debug.$canvas.height * 10)
     })
   }
